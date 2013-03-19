f075b6ecd328789edcc0a993e7879d446cb7dc83
Kyung Jun Oh <junswork@hotmail.com>
2013-03-19 05:33:48 -0700

    refactoring duplicate codes

--- SJF_SPB.cpp
+++ SJF_SPB.cpp
@@ -14,30 +14,43 @@ CPU sjf_spb(string args, vector<PCB*> pcbs){
 	CPU runningCPU;
 	deque<PCB*> readyQueue;
 	int deathCount;
+	float alpha;
 
-	if(args == "sjf"){
-		while(1){
-			deathCount = 0;
-			//Add all processes that can be run to ready queue
+	if(args = "spb" ) {
+		string s;
 
-			for(int i = 0; i < pcbs.size(); i++){
+		//Prompt the user for a value of alpha
+		do{
+			cout << "Please enter a value 0 < x <= 1 for alpha to weigh the value of the bursts: ";
+			cin >> s;
+			alpha = stof(s);
+		}while(alpha <= 0 || alpha > 1);
+	}
 
-				//If the process is dead, ignore it and take note
-				if(!pcbs.at(i)->getLife()){
-					deathCount++;
-				}
-				//If it can be run at this point in time and is not currently in the ready queue, add it to the ready queue.
-				//The ready queue should always have the first running object at the front of the queue. The order of all other elements is not set.
-				else if(pcbs.at(i)->getLife() && !pcbs.at(i)->isInReady() && pcbs.at(i)->getNextRunTime() <= runningCPU.getTime()){	
-					//Note that the process is in the ready queue
-					pcbs.at(i)->setReadyQ();
+	while(1){
+		deathCount = 0;
+		//Add all processes that can be run to ready queue
 
-					//Add the element to the ready queue
-					readyQueue.push_back(pcbs[i]);
-
-				}
+		for(int i = 0; i < pcbs.size(); i++){
+			
+			//If the process is dead, ignore it and take note
+			if(!pcbs.at(i)->getLife()){
+				deathCount++;
 			}
+			
+			//If it can be run at this point in time and is not currently in the ready queue, add it to the ready queue.
+			//The ready queue should always have the first running object at the front of the queue. The order of all other elements is not set.
+			else if(pcbs.at(i)->getLife() && !pcbs.at(i)->isInReady() && pcbs.at(i)->getNextRunTime() <= runningCPU.getTime()){	
+				//Note that the process is in the ready queue
+				pcbs.at(i)->setReadyQ();
 
+				//Add the element to the ready queue
+				readyQueue.push_back(pcbs[i]);
+
+			}
+		}
+
+		if(args == "sjf"){
 			//Find the element with the shortest runtime and put it at the front of the ready queue.
 			for(int k = 1; k < readyQueue.size();k++){
 
@@ -49,59 +62,8 @@ CPU sjf_spb(string args, vector<PCB*> pcbs){
 				}
 
 			}
-
-			//If there are no living processes, stop
-			if(deathCount >= pcbs.size()){
-				break;
-			}
-			//If there are still living processes, but none of them can be run, run an idle process for one unit of time
-			if(readyQueue.empty()){
-				PCB* idle = new PCB();
-				readyQueue.push_front(idle);
-			}
-			//Run the process and then remove it from the ready queue. The time alotted is as much time as the process needs to finish
-			//the current CPU burst
-			runningCPU.run(readyQueue.front(),readyQueue.front()->getNextCPU());			
-			readyQueue.pop_front();
-
 		}
-
-
-	}
-	//If the input is not sjf meaning it's SPB,
-	else{
-		string s;
-		float alpha;
-
-		//Prompt the user for a value of alpha
-		do{
-			cout << "Please enter a value 0 < x <= 1 for alpha to weigh the value of the bursts: ";
-			cin >> s;
-			alpha = stof(s);
-		}while(alpha <= 0 || alpha > 1);
-
-		while(1){
-			deathCount = 0;
-			//Add all processes that can be run to ready queue
-
-			for(int i = 0; i < pcbs.size(); i++){
-
-				//If the process is dead, ignore it and take note
-				if(!pcbs.at(i)->getLife()){
-					deathCount++;
-				}
-				//If it can be run at this point in time and is not currently in the ready queue, add it to the ready queue.
-				//The ready queue should always have the first running object at the front of the queue. The order of all other elements is not set.
-				else if(pcbs.at(i)->getLife() && !pcbs.at(i)->isInReady() && pcbs.at(i)->getNextRunTime() <= runningCPU.getTime()){	
-					//Note that the process is in the ready queue
-					pcbs.at(i)->setReadyQ();
-
-					//Add the element to the ready queue
-					readyQueue.push_back(pcbs[i]);
-
-				}
-			}
-
+		else { //if(args == "spb")
 			//Find the element with the shortest previous burst
 			//Initially the SPB is unknown as there are no previous bursts, set to 1
 			for(int k = 1; k < readyQueue.size();k++){
@@ -114,23 +76,26 @@ CPU sjf_spb(string args, vector<PCB*> pcbs){
 				}
 
 			}
-
-			//If there are no living processes, stop
-			if(deathCount >= pcbs.size()){
-				break;
-			}
-			//If there are still living processes, but none of them can be run, run an idle process for one unit of time
-			if(readyQueue.empty()){
-				PCB* idle = new PCB();
-				readyQueue.push_front(idle);
-			}
-			//Run the process and then remove it from the ready queue. The time alotted is as much time as the process needs to finish
-			//the current CPU burst
-			runningCPU.run(readyQueue.front(),readyQueue.front()->getNextCPU());
-			readyQueue.front()->setSPB(alpha, runningCPU.getLastRuntime());
-			readyQueue.pop_front();
-
 		}
+
+		//If there are no living processes, stop
+		if(deathCount >= pcbs.size()){
+			break;
+		}
+
+		//If there are still living processes, but none of them can be run, run an idle process for one unit of time
+		if(readyQueue.empty()){
+			PCB* idle = new PCB();
+			readyQueue.push_front(idle);
+		}
+
+		//Run the process and then remove it from the ready queue. The time alotted is as much time as the process needs to finish
+		//the current CPU burst
+		runningCPU.run(readyQueue.front(),readyQueue.front()->getNextCPU());
+		if(args = "spb") {
+			readyQueue.front()->setSPB(alpha, runningCPU.getLastRuntime());
+		}
+		readyQueue.pop_front();
 	}
 	return runningCPU;
 }
