792bda124cf6491c33c6553d154145f6cad5d4f1
Leslie Mui <leslie_mui@telus.net>
2013-03-19 17:22:38 -0700

    Apologies to jun, I misunderstood what the change to readyQ did.

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -76,26 +76,21 @@ CPU Algorithms(string args, vector<PCB*> pcbs){
 		for(int k = 1; k < readyQueue.size();k++){
 			if( args == "fifo" || args == "RR" ) {
 				if(readyQueue.front()->getNextRunTime() > readyQueue.at(k)->getNextRunTime()){
-					readyQueue.push_back(readyQueue.front());
 					readyQueue.push_front(*(readyQueue.begin()+k));
 					readyQueue.erase(readyQueue.begin()+k+1);
-					readyQueue.erase(readyQueue.begin()+1);
 				}
 			}
 			else if( args == "with_rude" || args == "with_pol" || args == "without" ) {
 				if(readyQueue.front()->getPRIO() > readyQueue.at(k)->getPRIO()){
-					//readyQueue.push_back(readyQueue.front());
+					cout << runningCPU.getTime() << "priority of front is: " << readyQueue.front()->getPRIO() << " back is: " << readyQueue.at(k)->getPRIO() << "\n";
 					readyQueue.push_front(*(readyQueue.begin()+k));
 					readyQueue.erase(readyQueue.begin()+k+1);
-					//readyQueue.erase(readyQueue.begin()+1);
 				}
 			} 
 			else { //if( args == "sjf" || args == "spb" ) {
 				if(readyQueue.front()->getNextCPU() > readyQueue.at(k)->getNextCPU()){
-					//readyQueue.push_back((readyQueue.front()));
 					readyQueue.push_front(*(readyQueue.begin()+k));
 					readyQueue.erase(readyQueue.begin()+k+1);
-					//readyQueue.erase(readyQueue.begin()+1);
 				}
 			}
 		}
